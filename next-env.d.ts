a077a41a30d6f65ae59eb9a293ab4e284c146498
BAHAA <lybha0954@gmail.com>
2026-08-18 19:15:11 +0300

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,7 +4,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-NEXT_PUBLIC_ADMIN_EMAIL=admin@jumlaati.iq
-NEXT_PUBLIC_ADMIN_PHONE=07700000000/07800000000
-NEXT_PUBLIC_ADMIN_PASSWORD=AdminPass@2026#   # كلمة مرور / رمز أمان المدير
-NEXT_PUBLIC_ADMIN_SECRET_KEY=IQ-Admin-2026-Secure  # المفتاح السري المباشر (Master Key)
