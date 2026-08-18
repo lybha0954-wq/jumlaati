5270c2d511306e51348f3979d4a85535102f544d
BAHAA <lybha0954@gmail.com>
2026-08-18 16:58:29 +0300

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,3 +4,6 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+NEXT_PUBLIC_ADMIN_EMAIL=lybha0954@gmail.com
+NEXT_PUBLIC_ADMIN_PHONE=07810224848
+NEXT_PUBLIC_ADMIN_SECRET_KEY=
