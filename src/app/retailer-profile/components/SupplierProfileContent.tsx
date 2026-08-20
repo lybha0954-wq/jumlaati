dd9b774fa3f15942b5a391e427f899b9623c30b2
BAHAA <lybha0954@gmail.com>
2026-08-20 22:53:15 +0000

    update

--- src/app/retailer-profile/components/SupplierProfileContent.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import RetailerProfileContent from './RetailerProfileContent';
-
-export default function SupplierProfileContent() {
-  return <RetailerProfileContent />;
-}
