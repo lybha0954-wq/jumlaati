ac15d2afa39c6f4696b75671ff4de99d17551942
BAHAA <lybha0954@gmail.com>
2026-08-29 22:18:30 +0300

    Update page.tsx

--- src/app/supplier-orders/page.tsx
+++ src/app/supplier-orders/page.tsx
@@ -1,11 +1,5 @@
-'use client';
-import AppLayout from '@/components/AppLayout';
 import SupplierOrdersContent from './components/SupplierOrdersContent';
 
-export default function SupplierOrdersPage() {
-  return (
-    <AppLayout>
-      <SupplierOrdersContent />
-    </AppLayout>
-  );
+export default function Page() {
+  return <SupplierOrdersContent />;
 }
