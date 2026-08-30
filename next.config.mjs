19d3fc8a684e20f41db0f063f63690a99cd80a14
BAHAA <lybha0954@gmail.com>
2026-08-31 01:42:07 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -64,8 +64,6 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
   },
 
-  // 3. تحسين حزمة التطبيق (تقليل حجم الكود المُرسل للمتصفح)
-  swcMinify: true,
 
   // 4. منع تحميل المكتبات غير المستخدمة (Tree Shaking)
   experimental: {
