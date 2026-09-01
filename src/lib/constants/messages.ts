9acaa59419c0224f123dc154f371338383ce158d
BAHAA <lybha0954@gmail.com>
2026-09-01 14:59:40 +0300

    Update messages.ts

---
+++ src/lib/constants/messages.ts
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGES = {
+  UNAUTHORIZED: "غير مصرح لك بالوصول",
+  NOT_FOUND: "العنصر غير موجود",
+  VALIDATION_ERROR: "البيانات المدخلة غير صحيحة",
+  SERVER_ERROR: "حدث خطأ في الخادم",
+};
