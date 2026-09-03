4cc198a9d69719606fe1d9c3c1572d69baaf98ea
BAHAA <lybha0954@gmail.com>
2026-09-03 12:18:50 +0300

    Update order.schema.ts

--- src/lib/validations/order.schema.ts
+++ src/lib/validations/order.schema.ts
@@ -1,9 +1,11 @@
 import * as z from "zod";
 
-export const createOrderSchema = z?.object({
-  items: z?.array(z?.object({
-    productId: z?.string(),
-    quantity: z?.coerce?.number()?.int()?.positive(),
-  }))?.min(1, "السلة فارغة"),
-  address: z?.string()?.min(5, "العنوان مفصل جداً"),
+export const createOrderSchema = z.object({
+  items: z.array(z.object({
+    productId: z.string(),
+    quantity: z.number().int().positive(),
+    wholesalerId: z.string(),
+    price: z.number().positive(),
+  })).min(1, "السلة فارغة"),
+  address: z.string().min(5, "العنوان مفصل جداً"),
 });
